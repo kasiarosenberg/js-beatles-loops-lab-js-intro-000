8b16da10a31f31bf9378140b3b2f3109d0e284a5
kate ROSENBERG <kate@digitalmachinations.com>
2018-02-27 19:00:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function theBeatlesPlay(arr1, arr2) {
+  var newArray = [];
+  for(var i=0; i<arr1.length; i++) {
+    newArray.push(arr1[i] + arr2[i]);
+  }
+  return newArray;
+}
+
+function helper(arr) {
+  for(var i=0; i<arr.length; i++) {
+    arr[i].unshift(arr[i] + "!!!");
+    arr[i-1].pop();
+  }
+  return arr;
+}
+function johnLennonFacts(arr) {
+  var newArray = [];
+  var counter = 0;
+  while(counter < arr.length) {
+    newArray.push(arr[counter] + "!!!");
+    counter++;
+  }
+  return newArray;
+}
+
+function iLoveTheBeatles(num) {
+  var arr = [];
+  do {
+    arr.push("I love the Beatles!");
+    num ++;
+  } while (num <15);
+  return "I love the Beatles!"
+}
